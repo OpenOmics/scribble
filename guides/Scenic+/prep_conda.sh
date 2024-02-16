058787a01a392ec082a615c95e233dbe98ba10b6
Tovah Markowitz <markowitzte@nih.gov>
2024-02-16 11:16:52 -0500

    Create prep_conda.sh

---
+++ guides/Scenic+/prep_conda.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+# This follows the instructions for setting up a conda environment on Biowulf as of 2/16/24.
+# Make sure to uses the package from the github site and only that as it ensures you have the correct versions of all required packages.
+
+python3 -m venv .scenicPlus_env
+source .scenicPlus_env/bin/activate
+pip install --upgrade pip
+
+git clone https://github.com/aertslab/scenicplus
+pip install -e scenicplus
+
+deactivate

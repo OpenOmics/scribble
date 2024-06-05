9da1c23502a4353dbda16516c08cbbfd09eda6a3
Tovah Markowitz <markowitzte@nih.gov>
2024-06-05 14:38:46 -0400

    Update pycistopic_model.py

--- guides/Scenic+/pycistopic_model.py
+++ guides/Scenic+/pycistopic_model.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 
-# source .scenicPlus_env/bin/activate
-
 import pandas as pd
 import numpy as np
 import pycisTopic
@@ -13,6 +11,8 @@ import pickle
 count_matrix2=pd.read_csv('peak_counts.csv', sep=',',index_col=0)
 cell_data =  pd.read_csv('ATAC_metadata.txt', sep='\t',index_col=0)
 
+# I chose "CellType" as the variable name to split the population on. 
+#     The variable name needs to be constant between the scRNA and scATAC metadata files and have the same set of potential values.
 # I needed to change the metadata information so that it would be identical between scATAC and scRNA
 cell_data['CellType'] = np.where(cell_data['dataset'] == 'AB4710', 'Th1', 'Polar')
 
@@ -22,6 +22,9 @@ cistopic_obj.add_cell_data(cell_data)
 # Try various n_topics ranging between 2 and 50 until you get what you like
 # n_cpu should be at least as many as the number of topics. Please do not
 # set n_cpus to more than 8, it will cause ray to error out!
+# Use "save_path" to save each model as separate pickles for later usage.
+# NOTE: rerunning an individual model over and over will give the same result, but different combinations of models
+#      may result in a different model chosen.
 models=run_cgs_models(cistopic_obj,
                     n_topics=[5,10,15,20,25,30,40,50],
                     n_cpu=8,
@@ -36,6 +39,6 @@ model = evaluate_models(models,
                         select_model=None,
                       return_model=True,
                       metrics=['Arun_2010','Cao_Juan_2009', 'Minmo_2011', 'loglikelihood'],
-                      plot_metrics=False,
+                      plot_metrics=False, # this errors out
                       save = 'pycistopic_model_evaluation.pdf')
 

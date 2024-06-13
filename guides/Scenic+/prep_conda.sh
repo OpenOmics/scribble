3da63548ed94aacab39951dea8c6c3a884a0aebf
Tovah Markowitz <markowitzte@nih.gov>
2024-06-13 10:04:18 -0400

    Update prep_conda.sh

--- guides/Scenic+/prep_conda.sh
+++ guides/Scenic+/prep_conda.sh
@@ -3,6 +3,8 @@
 # This follows the instructions for setting up a conda environment on Biowulf as of 2/16/24.
 # Make sure to uses the package from the github site and only that as it ensures you have the correct versions of all required packages.
 
+# You don't need this if using the singularity object.
+
 python3 -m venv .scenicPlus_env
 source .scenicPlus_env/bin/activate
 pip install --upgrade pip
